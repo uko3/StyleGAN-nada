37ef6c0865886bf0827231eb9547adb177276d74
uko3 <linar8kazan@mail.ru>
2025-08-16 23:35:37 +0300

    update

--- modules/trainer.py
+++ modules/trainer.py
@@ -153,8 +153,10 @@ class LatentStyleTrainer:
             self.losses['all'].append(loss_total.item())
 
             print(f"[{epoch}/{epochs}] Loss: {loss_total.item():.4f} | CLIP: {clip_loss:.4f} | L2: {l2_loss.item():.4f}")
-            print(lambda_clip, lambda_l2)
+            print(lambda_clip.item(), lambda_l2.item())
             print(self.lambda_t[0].item(), self.lambda_t[1].item())
+            print(self.lambda_t.item())
+            print(lambda_clip_init, math.log(lambda_clip_init))
             print('-'*20)
             # Visualization every 10 epochs
             if epoch % 10 == 0:
